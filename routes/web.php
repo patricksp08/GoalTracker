c0f85b052bbfe9003e6a05ff09e3e76b2c63268c
patricksp08 <patrickaborges@hotmail.com>
2026-04-24 18:50:06 -0300

    fix: corrigindo redirecionamento inicial e alterando nome do app

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,15 @@ use App\Http\Controllers\GoalController;
 use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
-
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::resource('users', UserController::class);
     Route::resource('goals', GoalController::class);
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
 
 require __DIR__.'/auth.php';
